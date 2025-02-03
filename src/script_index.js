e052fcdda481025b85f62f6c0a00e2dd88ffb02b
SHOCKff <156565597+SHOCKff@users.noreply.github.com>
2025-02-03 22:25:17 +0530

    Update script_index.js

--- src/script_index.js
+++ src/script_index.js
@@ -1,8 +1,9 @@
 var map = L.map('map');
-my_position = []; //for making  watch corodinaates  global
-var obj = null; //for making landed circle state  global
+my_position = []; 
+var obj = null; 
 var Route = null;
-// Add OpenStreetMap tile layer
+var lastOtherPosition = null; 
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
@@ -26,7 +27,7 @@ const intervalId = setInterval(async () => {
         error => { console.error(error) },
         { enableHighAccuracy: true }
     );
-}, 2000); // Runs every 2 seconds
+}, 2000); 
 
 // Show position of user when another user lands
 function landed() {
@@ -34,7 +35,7 @@ function landed() {
     map.setView([my_position[0], my_position[1]], 13);
     Landedmarker = L.marker([my_position[0], my_position[1]]).addTo(map);
     Landedmarker.bindPopup(`<b>Your Position:</b><br>Accuracy: ${my_position[2]} meters`).openPopup();
-    // circle
+    
     LandedCircle = L.circle([my_position[0], my_position[1]], {
         color: 'blue',
         fillColor: '#add8e6',
@@ -49,20 +50,28 @@ function landed() {
 socket.on("Server_data", (data) => {
     console.log("My socket id:", socket.id);
     console.log(data);
+    
     if (socket.id !== data[0]) { 
-     var end = L.latLng(data[1].latitude, data[1].longitude);
-    // remove landed marker if present
-     map.removeLayer(obj.Landedmarker);
-     map.removeLayer(obj.LandedCircle);
+        var end = L.latLng(data[1].latitude, data[1].longitude);
+
+        lastOtherPosition = data[1]; // Update last position
+
+        // remove landed marker if present
+        if (obj) {
+            map.removeLayer(obj.Landedmarker);
+            map.removeLayer(obj.LandedCircle);
+        }
     }
 
     var start = L.latLng(my_position[0], my_position[1]);
-    
-    // Add routing control
-    if(Route) {map.removeControl(Route)}
-    Route = L.Routing.control({
-        waypoints: [start, end],
-        show: false,
-    }).addTo(map);
-    console.log(Route)
+
+    // If route doesn't exist, create it. Otherwise, just update waypoints.
+    if (!Route) {
+        Route = L.Routing.control({
+            waypoints: [start, end],
+            show: false,
+        }).addTo(map);
+    } else {
+        Route.setWaypoints([start, end]); // **Only update waypoints instead of recreating the route**
+    }
 });
